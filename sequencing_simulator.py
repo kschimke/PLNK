34761b59781954beb0b3da020a3409f7fa314b32
Kayla Schimke <45776588+kschimke@users.noreply.github.com>
2021-10-28 10:48:41 -0700

    bug and formatting fixes

--- sequencing_simulator.py
+++ sequencing_simulator.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python3
+# Kayla Schimke, Christopher Vollmers
+
 import sys
 import os
 import time
 import glob
+import numpy as np
+import argparse
 
 def commandline():
-    import argparse
-    parser = argparse.ArgumentParser(description='Reorients/demuxes/trims consensus reads.',
-                                     add_help=True,
-                                     prefix_chars='-')
-    parser.add_argument('--input','-i', type=str, help='directory containing fast5 files')
-    parser.add_argument('--output','-o', type=str, help='path to directory to copy fast5s')
+    parser = argparse.ArgumentParser(description='Simulates an ONT sequencing run based on the metadata from input files',
+                                     add_help=True,prefix_chars='-',
+                                     usage='python3 sequencing_simulator.py -i [fast5s] -o [new directory]')
+    parser.add_argument('--input','-i', type=str, required=True, help='Directory containing fast5 files.')
+    parser.add_argument('--output','-o', type=str, default=os.getcwd(), help='Path to directory to copy fast5s. Defaults to current directory.')
 
     args = parser.parse_args()
     return args
@@ -17,22 +21,29 @@ def commandline():
 def main():
     args = commandline()
 
-    if not os.path.exists(outputFolder):
-        os.mkdir(outputFolder)
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
 
-    file_list = glob.glob(inputFolder + '/*.fast5')
+    file_list = glob.glob(args.input + '/*.fast5')
     file_list.sort(key=lambda x:os.path.getmtime(x))
 
-    previous=os.path.getmtime(file_list[0])
+    previous = os.path.getmtime(file_list[0])
+    avg_time = []
     for f5_path in file_list:
-        current=os.path.getmtime(f5_path)
+        current = os.path.getmtime(f5_path)
+        wait_time = current-previous
+        fast5_name = f5_path.split('/')[-1]
 
-        wait_time=current-previous
-        print('waiting %f seconds to move file %s' % (wait_time,f5_path.split('/')[-1]))
+        print('Waiting %f seconds to move file %s' %(wait_time,fast5_name))
         time.sleep(wait_time)
-        os.system('scp %s %s' %(f5_path,outputFolder+fast5))
+        os.system('scp %s %s' %(f5_path,args.output+fast5_name))
 
-        previous=current
+        if wait_time > 0:
+            avg_time.append(wait_time)
+
+        previous = current
+
+    print('Average time between files: %f seconds' %(np.median(avg_time)))
 
 if __name__ == '__main__':
     main()
