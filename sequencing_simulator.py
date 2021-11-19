3473a0a9c3aa6f22f36670dfa93ede6448984fa2
Kayla Schimke <45776588+kschimke@users.noreply.github.com>
2021-11-19 12:45:02 -0800

    added --rate option
    --rate -r  Allows the user to define the number of minutes between fast5 file transfers instead of extrapolating a wait time from the files' metadata.

--- sequencing_simulator.py
+++ sequencing_simulator.py
@@ -14,6 +14,7 @@ def commandline():
                                      usage='python3 sequencing_simulator.py -i [fast5s] -o [new directory]')
     parser.add_argument('--input','-i', type=str, required=True, help='Directory containing fast5 files.')
     parser.add_argument('--output','-o', type=str, default=os.getcwd(), help='Path to directory to copy fast5s. Defaults to current directory.')
+    parser.add_argument('--rate', '-r', type=float, default=None, help='Number of minutes between fast5 file transfers. Defaults to using the metadata.')
 
     args = parser.parse_args()
     return args
@@ -27,23 +28,34 @@ def main():
     file_list = glob.glob(args.input + '/*.fast5')
     file_list.sort(key=lambda x:os.path.getmtime(x))
 
-    previous = os.path.getmtime(file_list[0])
-    avg_time = []
-    for f5_path in file_list:
-        current = os.path.getmtime(f5_path)
-        wait_time = current-previous
-        fast5_name = f5_path.split('/')[-1]
+    if args.rate:
+        print('Transfer rate set to %f minute(s)' %(args.rate))
+        wait_time = args.rate * 60
+        for f5_path in file_list:
+             f5_name = f5_path.split('/')[-1]
 
-        print('Waiting %f seconds to move file %s' %(wait_time,fast5_name))
-        time.sleep(wait_time)
-        os.system('scp %s %s' %(f5_path,args.output+fast5_name))
+             print('Transferring %s' %(f5_name))
+             time.sleep(wait_time)
+             os.system('scp %s %s' %(f5_path,args.output+f5_name))
 
-        if wait_time > 0:
-            avg_time.append(wait_time)
+    else:
+        previous = os.path.getmtime(file_list[0])
+        avg_time = []
+        for f5_path in file_list:
+            f5_name = f5_path.split('/')[-1]
+            current = os.path.getmtime(f5_path)
+            wait_time = current-previous
 
-        previous = current
+            print('Waiting %f seconds to move file %s' %(wait_time,f5_name))
+            time.sleep(wait_time)
+            os.system('scp %s %s' %(f5_path,args.output+f5_name))
 
-    print('Average time between files: %f seconds' %(np.median(avg_time)))
+            if wait_time > 0:
+                avg_time.append(wait_time)
+
+            previous = current
+
+        print('Average time between files: %f seconds' %(np.median(avg_time)))
 
 if __name__ == '__main__':
     main()
